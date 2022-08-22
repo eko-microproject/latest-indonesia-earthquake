5b9159fae97f4acbc2cfbe9215fde7d28579b4aa
ekowj <eko.wijaya03@gmail.com>
2022-08-23 01:12:54 +0700

    Edit 'setup.py'

--- setup.py
+++ setup.py
@@ -4,14 +4,14 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="earthquake-id-warning",                     # This is the name of the package
-    version="0.0.2",                        # The initial release version
-    author="Heru Setiawan",                     # Full name of the author
-    author_email="lenterabots@gmail.com",
-    description="this project will report the latest earthquake in Indonesia from BMKG",
+    name="latestearthquake-id",                     # This is the name of the package
+    version="0.0.1",                        # The initial release version
+    author="Eko Wijaya",                     # Full name of the author
+    author_email="microprojects97@gmail.com",
+    description="This package will get the latest earthquake from BMKG | Meteorological, Climatological, and Geophysical Agency",
     long_description=long_description,      # Long description read from the the readme file
     long_description_content_type="text/markdown",
-    url="https://github.com/goestoremote/indonesia-earthquake",
+    url="https://github.com/eko-microproject/latest-indonesia-earthquake",
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
